06796feccc27781e0f2e7049f7dda18118cfb285
pinguytaz <webmaster@pinguytaz.net>
2024-08-21 20:01:35 +0200

    Se sube la codificacion HEXADECIMAL

--- README.md
+++ README.md
@@ -4,24 +4,34 @@ Extensiones para utilizar en AppInventor.
 
 <BR>
 
-- **[Utiles](Utiles)**  
+**[Utiles](Utiles)**  
 Conjunto de extensiones para la ayuda en la depuración, cifrados y obtencion de información del sistema.  
-
-- **Logs**
+  
+- **Logs**  
 Nos permite escribir mensajes en LogCat  
 En la funcion Log() definiremos el texto a enviar y el tipo de log: Verbose, Debug, Info, Advertencia y Error  
-<CENTER><img src=Imagenes/LlamadaLog.png /></CENTER>  
+
+<p><CENTER><img src=Imagenes/LlamadaLog.png /></CENTER> </p>
   
-- **Codifica**
+- **Codifica**  
 Funciones para codificar Strings, ya sean Hash como MD5 y SHA-256, conversiones(más adelante) a Base64 y Base 16 y encriptación(más adelante).  
 Tenemos un componente evento que captura las posibles excepciónes de las funciones:
-<CENTER><img src=Imagenes/Evento_OcurreUnError.png /></CENTER>  
+<p><center><img src=Imagenes/Evento_OcurreUnError.png /></center> </p>
   
-       HASH (MD5 y SHA-256) a las que les pasamos el texto para que nos retorne una cadena con el HASH  
-<CENTER> <img src=Imagenes/FuncionesHASH.png /> </CENTER>
-<CENTER>MD5 16Bytes y SHA-256 32Bytes</CENTER>  
+            HASH: MD5(16 bytes) y SHA-256(32 bytes) se les pasamos el texto y retorna la cadena con el HASH  
+  
+<p><CENTER> <img src=Imagenes/FuncionesHASH.png /></CENTER></p>
+  
+            Codifica Hexadecimal y Base 64  
+  
+<p><CENTER> <img src=Imagenes/CodificacionHEX.png /></CENTER></p>
   
 
+<BR>
+
+**[Ejemplos](Ejemplos)**  
+Ejemplos de extensiones realizadas para los POST de https://www.pinguytaz.net  
+  
 <br><br>
 
 __Website__: <https://www.pinguytaz.net>
--- Utiles/src/pinguytaz/net/utiles/Codifica.java
+++ Utiles/src/pinguytaz/net/utiles/Codifica.java
@@ -3,7 +3,8 @@
  *  WEB: www.pinguytaz.net
  *
  *  Descripción: Funciones de codificación: Hash: MD5, SHA-256 
- *                                          Proximo Codifica: Base64, Base16
+ *                                          Codifica: Hexadecimal,
+ *                                          Proximo Base64
  *                                          Proximo Encriptacion: XOR, ¿?
  * 
  *  Version 1.0 Agosto 2024
@@ -45,56 +46,85 @@ public class Codifica extends AndroidNonvisibleComponent {
        super(container.$form());
    }   
 
-  // Funciones Hash.
-  // MD5
-  @SimpleFunction(description = "Funcion que convierte un String a un MD5")
-  public String Hash_MD5(String texto) {
-      String resultado = "0102030405060708090A0B0C0D0E0F";
+   // Funciones Hash.
+   // MD5
+   @SimpleFunction(description = "Funcion que convierte un String a un MD5")
+   public String Hash_MD5(String texto) {
+       String resultado = "0102030405060708090A0B0C0D0E0F";
+ 
+       try {
+          MessageDigest md5 = MessageDigest.getInstance("MD5");
+          md5.update(texto.getBytes());
+          byte[] digest = md5.digest();
+          // Convertimos el array de bytes a String en formato Hexadeciaml
+          StringBuffer hexString = new StringBuffer();
+          for (int i = 0;i<digest.length;i++) {
+             hexString.append(String.format("%02x", (0xFF & digest[i])));
+          }
+          resultado = hexString.toString();  
+       } catch(Exception e) { OcurreUnError(e.getMessage(),"En MD5" ); }
+ 
+       return resultado;
+   }
 
-      try {
-         MessageDigest md5 = MessageDigest.getInstance("MD5");
-         md5.update(texto.getBytes());
-         byte[] digest = md5.digest();
-         // Convertimos el array de bytes a String en formato Hexadeciaml
-         StringBuffer hexString = new StringBuffer();
-         for (int i = 0;i<digest.length;i++) {
-            //hexString.append(Integer.toHexString(0xFF & digest[i]));
-            hexString.append(String.format("%02x", (0xFF & digest[i])));
-         }
-         resultado = hexString.toString();  
-      //} catch(NoSuchAlgorithmException e) { OcurreUnError(e.getMessage(),"En MD5" ); }
-      } catch(Exception e) { OcurreUnError(e.getMessage(),"En MD5" ); }
-
-     return resultado;
-  }
-
-  // SHA256
-  @SimpleFunction(description = "Funcion que convierte un String a un Hash SHA256 (SHA2 de 256 y 64 rondas")
-  public String Hash_SH256(String texto) {
-      String resultado = "0102030405060708090A0B0C0D0E0F1112131415161718191A1B1C1D1E1F";
-
-      try {
-         MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-         sha256.update(texto.getBytes());
-         byte[] digest = sha256.digest();
-         // Convertimos el array de bytes a String en formato Hexadeciaml
-         StringBuffer hexString = new StringBuffer();
-         for (int i = 0;i<digest.length;i++) {
-            //hexString.append(Integer.toHexString(0xFF & digest[i]));
-            hexString.append(String.format("%02x", (0xFF & digest[i])));
-         }
-         resultado = hexString.toString();  
-      } catch(Exception e) { OcurreUnError(e.getMessage(),"En SHA256" ); }
-
-     return resultado;
-  }
-
-  // Evento información de error
-  @SimpleEvent(description = "Dispara evento de Error")
-  public void OcurreUnError(String error, String funcion) {
-     String texto = "Ocurre un error en " + funcion + " tipo: " + error;
-     Log.e(TAG,texto);
-     EventDispatcher.dispatchEvent(this, "OcurreUnError", error, funcion);
-  }
-
+   // SHA256
+   @SimpleFunction(description = "Funcion que convierte un String a un Hash SHA256 (SHA2 de 256 y 64 rondas")
+   public String Hash_SH256(String texto) {
+       String resultado = "0102030405060708090A0B0C0D0E0F1112131415161718191A1B1C1D1E1F";
+ 
+       try {
+          MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+          sha256.update(texto.getBytes());
+          byte[] digest = sha256.digest();
+          // Convertimos el array de bytes a String en formato Hexadeciaml
+          StringBuffer hexString = new StringBuffer();
+          for (int i = 0;i<digest.length;i++) {
+             //hexString.append(Integer.toHexString(0xFF & digest[i]));
+             hexString.append(String.format("%02x", (0xFF & digest[i])));
+          }
+          resultado = hexString.toString();  
+       } catch(Exception e) { OcurreUnError(e.getMessage(),"En SHA256" ); }
+ 
+      return resultado;
+   }
+ 
+   // Base16  a Hexadecimal
+   @SimpleFunction(description = "Funcion que convierte un String en Hexadecimal")
+   public String CodiA_Hex(String texto) {
+       String resultado = "En Hexadecimal la cadena de texto";
+       StringBuffer hexString = new StringBuffer();
+       for (int i = 0;i<texto.length();i++) {
+             hexString.append(String.format("%02x", (0xFF & texto.charAt(i))));
+       }
+       resultado = hexString.toString();
+       return resultado;
+   } 
+ 
+   @SimpleFunction(description = "Funcion que convierte un String HEX a cadena de caracteres")
+   public String CodiDe_Hex(String hex) {
+   char[] ascii = new char[hex.length() / 2]; 
+       for (int i = 0; i < hex.length(); i += 2) {  // Avanzamos de 2 en dos.
+          String hexByte = hex.substring(i, i + 2);  // Obtiene dos caracteres
+          int asciiValue = Integer.parseInt(hexByte, 16); // Convertir hexadecimal a decimal
+          ascii[i / 2] = (char) asciiValue; // Convertir decimal a caracter ASCII
+      }
+      String resultado = new String(ascii);
+      return resultado;
+   } 
+ 
+   // Base64
+   /*@SimpleFunction(description = "Funcion que convierte un String en Base 64")
+   public String Codi_Base64(String texto) {
+       String resultado = "El Codigo en Base 64";
+       return resultado;
+   } 
+*/
+ 
+   // Evento información de error
+   @SimpleEvent(description = "Dispara evento de Error")
+   public void OcurreUnError(String error, String funcion) {
+      String texto = "Ocurre un error en " + funcion + " tipo: " + error;
+      Log.e(TAG,texto);
+      EventDispatcher.dispatchEvent(this, "OcurreUnError", error, funcion);
+   }
 }
