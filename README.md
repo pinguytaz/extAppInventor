5e53df914095eaf1050446e3d582cfb124c8c6cd
pinguytaz <webmaster@pinguytaz.net>
2024-08-21 20:50:45 +0200

    Subimos los Base64 y la correccion de HEX que no teneian captura de excepciones.

--- README.md
+++ README.md
@@ -4,32 +4,33 @@ Extensiones para utilizar en AppInventor.
 
 <BR>
 
-**[Utiles](Utiles)**  
+__**[Utiles](Utiles)**__  
 Conjunto de extensiones para la ayuda en la depuración, cifrados y obtencion de información del sistema.  
   
-- **Logs**  
+**Logs**  
 Nos permite escribir mensajes en LogCat  
 En la funcion Log() definiremos el texto a enviar y el tipo de log: Verbose, Debug, Info, Advertencia y Error  
 
-<p><CENTER><img src=Imagenes/LlamadaLog.png /></CENTER> </p>
+<p align="center"><img src=Imagenes/LlamadaLog.png /></p>
   
-- **Codifica**  
+**Codifica**  
 Funciones para codificar Strings, ya sean Hash como MD5 y SHA-256, conversiones(más adelante) a Base64 y Base 16 y encriptación(más adelante).  
 Tenemos un componente evento que captura las posibles excepciónes de las funciones:
-<p><center><img src=Imagenes/Evento_OcurreUnError.png /></center> </p>
+<center><img src=Imagenes/Evento_OcurreUnError.png /></p>
   
-            HASH: MD5(16 bytes) y SHA-256(32 bytes) se les pasamos el texto y retorna la cadena con el HASH  
+- **HASH: MD5(16 bytes) y SHA-256(32 bytes)** se les pasamos el texto y retorna la cadena con el HASH  
   
-<p><CENTER> <img src=Imagenes/FuncionesHASH.png /></CENTER></p>
+<p align="center"> <img src=Imagenes/FuncionesHASH.png /></p>
   
-            Codifica Hexadecimal y Base 64  
+- **Codifica Hexadecimal y Base 64** Se pasa la cadena de texto o HEXadecimal/Base64  
   
-<p><CENTER> <img src=Imagenes/CodificacionHEX.png /></CENTER></p>
+<p align="center"> <img src=Imagenes/CodificacionHEX.png /></p>  
+<p align="center"> <img src=Imagenes/CodificacionBase64.png /></p>  
   
 
 <BR>
 
-**[Ejemplos](Ejemplos)**  
+_**[Ejemplos](Ejemplos)**_  
 Ejemplos de extensiones realizadas para los POST de https://www.pinguytaz.net  
   
 <br><br>
--- Utiles/src/pinguytaz/net/utiles/Codifica.java
+++ Utiles/src/pinguytaz/net/utiles/Codifica.java
@@ -3,8 +3,7 @@
  *  WEB: www.pinguytaz.net
  *
  *  Descripción: Funciones de codificación: Hash: MD5, SHA-256 
- *                                          Codifica: Hexadecimal,
- *                                          Proximo Base64
+ *                                          Codifica: Hexadecimal, Base64
  *                                          Proximo Encriptacion: XOR, ¿?
  * 
  *  Version 1.0 Agosto 2024
@@ -71,7 +70,6 @@ public class Codifica extends AndroidNonvisibleComponent {
    @SimpleFunction(description = "Funcion que convierte un String a un Hash SHA256 (SHA2 de 256 y 64 rondas")
    public String Hash_SH256(String texto) {
        String resultado = "0102030405060708090A0B0C0D0E0F1112131415161718191A1B1C1D1E1F";
- 
        try {
           MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
           sha256.update(texto.getBytes());
@@ -92,33 +90,52 @@ public class Codifica extends AndroidNonvisibleComponent {
    @SimpleFunction(description = "Funcion que convierte un String en Hexadecimal")
    public String CodiA_Hex(String texto) {
        String resultado = "En Hexadecimal la cadena de texto";
-       StringBuffer hexString = new StringBuffer();
-       for (int i = 0;i<texto.length();i++) {
-             hexString.append(String.format("%02x", (0xFF & texto.charAt(i))));
-       }
-       resultado = hexString.toString();
+       try {
+          StringBuffer hexString = new StringBuffer();
+          for (int i = 0;i<texto.length();i++) {
+                hexString.append(String.format("%02x", (0xFF & texto.charAt(i))));
+          }
+          resultado = hexString.toString();
+       } catch(Exception e) { OcurreUnError(e.getMessage(),"En CodiAHEX" ); }
        return resultado;
    } 
  
    @SimpleFunction(description = "Funcion que convierte un String HEX a cadena de caracteres")
    public String CodiDe_Hex(String hex) {
-   char[] ascii = new char[hex.length() / 2]; 
-       for (int i = 0; i < hex.length(); i += 2) {  // Avanzamos de 2 en dos.
-          String hexByte = hex.substring(i, i + 2);  // Obtiene dos caracteres
-          int asciiValue = Integer.parseInt(hexByte, 16); // Convertir hexadecimal a decimal
-          ascii[i / 2] = (char) asciiValue; // Convertir decimal a caracter ASCII
-      }
-      String resultado = new String(ascii);
+       String resultado ="Datos en HEX";
+       try {
+          char[] ascii = new char[hex.length() / 2]; 
+          for (int i = 0; i < hex.length(); i += 2) {  // Avanzamos de 2 en dos.
+             String hexByte = hex.substring(i, i + 2);  // Obtiene dos caracteres
+             int asciiValue = Integer.parseInt(hexByte, 16); // Convertir hexadecimal a decimal
+             ascii[i / 2] = (char) asciiValue; // Convertir decimal a caracter ASCII
+         }
+         resultado = new String(ascii);
+      } catch(Exception e) { OcurreUnError(e.getMessage(),"En CodiDeHEX" ); }
       return resultado;
    } 
  
    // Base64
-   /*@SimpleFunction(description = "Funcion que convierte un String en Base 64")
-   public String Codi_Base64(String texto) {
-       String resultado = "El Codigo en Base 64";
-       return resultado;
+   @SimpleFunction(description = "Funcion que convierte un String en Base 64")
+   public String CodiA_Base64(String texto) {
+      String resultado = "La Cadena";
+      try {
+         byte[] bytesTexto = texto.getBytes("UTF-8");
+         byte[] cadenaBase64 = Base64.encode(bytesTexto, Base64.DEFAULT);
+         resultado = new String(cadenaBase64);
+      } catch(Exception e) { OcurreUnError(e.getMessage(),"En CodiABase64" ); }
+      return resultado;
    } 
-*/
+   @SimpleFunction(description = "Funcion que convierte de base 64 a String")
+   public String CodiDe_Base64(String base64) {
+      String resultado = "La cadena despues de 64";
+      try {
+         byte[] cadena = Base64.decode(base64, Base64.DEFAULT);
+         resultado = new String(cadena);
+      } catch(Exception e) { OcurreUnError(e.getMessage(),"En CodiDeBase64" ); }
+      return resultado;
+   } 
+
  
    // Evento información de error
    @SimpleEvent(description = "Dispara evento de Error")
